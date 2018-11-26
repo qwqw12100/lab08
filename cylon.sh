62569a7b631452a07ba7a3f3063ed064084c6be5
qwqw12100 <qwqw12100@github.com>
2018-11-26 03:03:45 +0000

    “setup”

--- cylon.sh
+++ cylon.sh
@@ -5,10 +5,11 @@ do
         gpio mode $value out
 done
 
-while true
-do
-    gpio write 0 1 
-	gpio write 1 0 
+
+for ((i=1;i<10;))
+ do 
+        gpio write 0 1 
+   	gpio write 1 0 
 	gpio write 2 0 
 	gpio write 3 0
 	sleep 0.25
--- setbits.sh
+++ setbits.sh
@@ -2,34 +2,39 @@
 
 for value in {0..3}
 do
-        gpio mode $value out
+   gpio mode $value out
 done
 
-if (($1 & 1))
+
+for value in {0..3}
+do
+
+if (($1 >> 0 & 1))
 then
   gpio write 0 1
 else
   gpio write 0 0
 fi
 
-if ( ($1 >> 1) & 1 )
+if (($1 >> 1 & 1))
 then
   gpio write 1 1
 else
   gpio write 1 0
 fi
 
-if ( ($1>>2) & 1 )
+if (($1 >> 2 & 1))
 then
   gpio write 2 1
 else
   gpio write 2 0
 fi
 
-if (($1>>3) & 1 )
+if (($1>>3 & 1 ))
 then
   gpio write 3 1
 else
   gpio write 3 0
 fi
 
+done
